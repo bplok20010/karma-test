d5cc5107a41278bf0235b55349599edb6ccbca58
nobo <505931977@qq.com>
2017-03-01 15:15:11 +0800

    $npm_package_version

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 import isNum from '../src/index'
 import add from '../src/add'
+import max from '../src/max'
 
 describe('index.js: ', () => {
     it('isNum() should work fine.', () => {
@@ -8,5 +9,8 @@ describe('index.js: ', () => {
     })
     it('add() should work fine.', () => {
         expect(add(4)).toBe(5)
+    })
+    it('max() should work fine.', () => {
+        expect(max(7, 1, 5, 8, 9, 14, 2, 0)).toBe(14)
     })
 })
